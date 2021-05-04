4a0b011b6801bf17df10aaa3c5d69367726bcd9c
tunde2018 <tunadng@yahoo.com>
2021-05-04 22:18:16 +0100

    committing initial framework

---
+++ src/test/java/co/uk/next/StepDefinitions/AccountSearchSteps.java
@@ -0,0 +1,89 @@
+package co.uk.next.StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AccountSearchSteps {
+
+    @Given("I navigate to next homepage")
+    public void i_navigate_to_next_homepage() {
+
+    }
+
+    @When("I click on {string} button")
+    public void i_click_on_button(String string) {
+
+    }
+
+    @When("I select {string} from Title dropdown")
+    public void i_select_from_Title_dropdown(String string) {
+
+    }
+
+    @When("I enter {string} into the Name field")
+    public void i_enter_into_the_Name_field(String string) {
+
+    }
+
+    @When("I enter {string} into the Last name text box")
+    public void i_enter_into_the_Last_name_text_box(String string) {
+
+    }
+
+    @When("I enter {string} into the Email field")
+    public void i_enter_into_the_Email_field(String string) {
+
+    }
+
+    @When("I enter {string} into the Password text box")
+    public void i_enter_into_the_Password_text_box(String string) {
+
+    }
+
+    @When("I enter {string} in the Date of birth searchfield")
+    public void i_enter_in_the_Date_of_birth_searchfield(String string) {
+
+    }
+
+    @When("I enter {string} into the Telephone text box")
+    public void i_enter_into_the_Telephone_text_box(String string) {
+
+    }
+
+    @When("I enter {string} into the text box")
+    public void i_enter_into_the_text_box(String string) {
+
+    }
+
+    @When("I click on FIND button")
+    public void i_click_on_FIND_button() {
+
+    }
+
+    @Then("I select address from {string} dropdown")
+    public void i_select_address_from_dropdown(String string) {
+
+    }
+
+    @Then("I click on click {string} button")
+    public void i_click_on_click_button(String string) {
+
+    }
+
+    @Then("welcome to next is displayed")
+    public void welcome_to_next_is_displayed() {
+
+    }
+
+    @Then("I click on {string} tab button")
+    public void i_click_on_tab_button(String string) {
+
+    }
+
+    @Then("my account is displayed")
+    public void my_account_is_displayed() {
+
+    }
+
+}
---
+++ src/test/java/co/uk/next/features/CustomerCanRegisterNewAccountNow.feature
@@ -0,0 +1,27 @@
+Feature: New account
+  As customer
+  I want to be able to open account
+  So I buy items of my choice
+
+  Scenario Outline:  Open new account
+    Given I navigate to next homepage
+    When I click on "<REGISTER NOW>" button
+    And I select "<MyTitle>" from Title dropdown
+    And I enter "<FirstName>" into the Name field
+    And  I enter "<LastName>" into the Last name text box
+   When I enter "<Email>" into the Email field
+    And I enter "<Password>" into the Password text box
+    And I enter "<DateOfBirth>" in the Date of birth searchfield
+    And I enter "<ContactTelephone>" into the Telephone text box
+    And I enter "<House No./ Name>" into the text box
+    And I enter "<Postcode>" into the text box
+    And I click on FIND button
+    Then I select address from "<PleaseSelectAddress>" dropdown
+    And I click on click "<GREAT,REGISTERMYACCOUNT>" button
+    Then welcome to next is displayed
+    And I click on "<ShopNow>" tab button
+    And my account is displayed
+
+    Examples:
+      | REGISTER NOW | MyTitle | FirstName | LastName | Email | Password | DateOfBirth | ContactTelephone | House No./ Name | Postcode | PleaseSelectAddress | GREAT,REGISTERMYACCOUNT | ShopNow |
+      |Register now  |   Mr      |  Tunde         |  Ade        | tunadng@yahoo.com      |   07809127596       |  45           |   M24 2FZ               |45 Fern Close Middleton Manchester                 |          |                     |                         |         |
